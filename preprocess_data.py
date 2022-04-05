e440321cd8ee4f38636f9afa56aed95f18f7699d
meso272 <liujinyang1201@126.com>
2022-04-04 21:40:40 -0700

    init

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,10 +5,9 @@ import sys
 datafolder=sys.argv[1]
 
 for file in os.listdir(datafolder):
-    if file.split(".")[-1]!=".d64":
+    if file.split(".")[-1]!="d64":
         continue
     filepath=os.path.join(datafolder,file)
     newfilename=file.split(".")[0]+".f32"
     newfilepath=os.path.join(datafolder,newfilename)
-    a=np.fromfile(filepath,dtype=np.double).astype(np.float32)
-    a.tofile(newfilepath)
+    np.fromfile(filepath,dtype=np.double).astype(np.float32).tofile(newfilepath)
