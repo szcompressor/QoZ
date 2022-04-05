02c2a9989357db68550336e528d3f4e69965484d
meso272 <liujinyang1201@126.com>
2022-04-05 16:10:18 -0700

    111

--- generate_config.py
+++ generate_config.py
@@ -1,13 +1,13 @@
 import sys
 import os 
 
-tt=sys.argv[1]
+#tt=sys.argv[1]
 tuning_target_dict={"psnr":"TUNING_TARGET_RD","cr":"TUNING_TARGET_CR","ssim":"TUNING_TARGET_SSIM","ac":"TUNING_TARGET_AC"}
-tuning_target=tuning_target_dict[tt]
+
 if not os.path.exists("configs"):
     os.mkdir("configs")
 datasets=["cesm","miranda","nyx","scale","hurricane"]
-
+targets=["cr","psnr","ssim","ac"]
 for dataset in datasets:
     testLorenzo=0
     if dataset=="cesm":
@@ -22,8 +22,10 @@ for dataset in datasets:
         autoTuningRate=0.005
         predictorTuningRate=0.005
         levelwisePredictionSelection=4
+    for target in targets:
 
-    configstr="[GlobalSettings]\nCmprAlgo = ALGO_INTERP_LORENZO \ntuningTarget = %s \n[AlgoSettings]\nautoTuningRate = %f \npredictorTuningRate= %f \nlevelwisePredictionSelection = %d \nmaxStep = %d \ntestLorenzo = %d \nsampleBlockSize = %d \nverbose = 0 \n" % \
-    (tuning_target,autoTuningRate,predictorTuningRate,levelwisePredictionSelection,maxStep,testLorenzo,sampleBlockSize) 
-    with open(os.path.join("configs", "%s_%s.config" % (dataset,tt) ),"w") as f:
-        f.write(configstr)
+        tuning_target=tuning_target_dict[target]
+        configstr="[GlobalSettings]\nCmprAlgo = ALGO_INTERP_LORENZO \ntuningTarget = %s \n[AlgoSettings]\nautoTuningRate = %f \npredictorTuningRate= %f \nlevelwisePredictionSelection = %d \nmaxStep = %d \ntestLorenzo = %d \nsampleBlockSize = %d \nverbose = 0 \n" % \
+        (tuning_target,autoTuningRate,predictorTuningRate,levelwisePredictionSelection,maxStep,testLorenzo,sampleBlockSize) 
+        with open(os.path.join("configs", "%s_%s.config" % (dataset,target) ),"w") as f:
+            f.write(configstr)
