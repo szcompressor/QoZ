2ccd44aa48d44f79239e5a8522d7b84190ffa2c9
Jliu-1 <jliu217@central.uh.edu>
2024-10-30 16:48:07 -0500

    bugfix

--- include/QoZ/api/impl/SZImplOMP.hpp
+++ include/QoZ/api/impl/SZImplOMP.hpp
@@ -5,8 +5,9 @@
 #include <cmath>
 #include <memory>
 
-
+#ifdef _OPENMP
 #include "omp.h"
+#endif
 
 template<class T, QoZ::uint N>
 char *SZ_compress_OMP(QoZ::Config &conf, T *data, size_t &outSize) {
